3d87be9aec0e3d71a3aec94c988fc6f88762552a
manoj <manoj7gsmith@gmail.com>
2022-03-05 18:40:49 +0530

    changed the sstaticfiles_dirs in settings.py

--- animalBlog/settings.py
+++ animalBlog/settings.py
@@ -119,9 +119,16 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
+
+if DEBUG:
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+   ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL= '/images/'
 MEDIA_ROOT= os.path.join(BASE_DIR, 'static/images')
